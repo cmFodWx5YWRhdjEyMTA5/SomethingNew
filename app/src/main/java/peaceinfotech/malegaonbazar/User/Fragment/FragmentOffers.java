9970b88916ffa1a42f774120da0be5344e05a57a
SiddhantJain <siddhantjain2317@gmail.com>
2019-03-01 11:12:14 +0530

    popup windows chnages

--- app/src/main/java/peaceinfotech/malegaonbazar/User/Fragment/FragmentOffers.java
+++ app/src/main/java/peaceinfotech/malegaonbazar/User/Fragment/FragmentOffers.java
@@ -149,7 +149,8 @@ public class FragmentOffers extends Fragment implements OnMapReadyCallback,Locat
                //display the popup window
                 popupWindow.showAtLocation(layup, Gravity.CENTER, 0, 0);
 
-
+                popupWindow.setBackgroundDrawable(null);
+                
                //close the popup window on button click
                 closePopupBtn.setOnClickListener(new View.OnClickListener() {
                     @Override
