7b12e17348618eabfbeb12d173e9499018df0e1d
SiddhantJain <siddhantjain2317@gmail.com>
2019-03-04 12:30:30 +0530

    offers Informatio Activity

--- app/src/main/java/peaceinfotech/malegaonbazar/User/UI/OfferInformationActivity.java
+++ app/src/main/java/peaceinfotech/malegaonbazar/User/UI/OfferInformationActivity.java
@@ -2,11 +2,15 @@ package peaceinfotech.malegaonbazar.User.UI;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.view.WindowManager;
 
 import peaceinfotech.malegaonbazar.R;
 
 public class OfferInformationActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,7 +18,18 @@ public class OfferInformationActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_offer_information);
 
+        toolbar=findViewById(R.id.offer_tool);
 
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        getSupportActionBar().setTitle("Offers Information");
 
     }
 }
